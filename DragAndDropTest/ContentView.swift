4c1b6b328c001cd65abf5271b14b071eec9191be
Kliffyyy <88235200+Kliffyyy@users.noreply.github.com>
2023-04-08 10:46:02 +0800

    Initial Commit
    Added basic drag and drop from hacking with swift

--- DragAndDropTest/ContentView.swift
+++ DragAndDropTest/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    let sports = ["figure.badminton", "figure.cricket", "figure.fencing"]
+    @State private var dropImage = Image(systemName: "photo")
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                ForEach(sports, id: \.self) { sport in
+                    Image(systemName: sport)
+                        .frame(minWidth: 50, minHeight: 50)
+                        .background(.red)
+                        .foregroundColor(.white)
+                        .draggable(Image(systemName: sport)) {
+                            Label("", systemImage: sport)
+                        }
+                        .cornerRadius(10)
+                }
+            }
+            .frame(minWidth: 300, minHeight: 70)
+            .background(.gray)
+            .cornerRadius(10)
+
+            dropImage
+                .frame(width: 150, height: 150)
+                .background(.green)
+                .foregroundColor(.white)
+                .dropDestination(for: Image.self) { items, location in
+                    dropImage = items.first ?? Image(systemName: "photo")
+                    return true
+                }
+                .cornerRadius(10)
         }
-        .padding()
     }
 }
 
