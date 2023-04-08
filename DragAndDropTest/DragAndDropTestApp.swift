46779402d94f221a818cc84b335ac46ecb525ee8
Kliffyyy <88235200+Kliffyyy@users.noreply.github.com>
2023-04-08 09:59:25 +0800

    Initial Commit

---
+++ DragAndDropTest/DragAndDropTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndDropTestApp.swift
+//  DragAndDropTest
+//
+//  Created by klifton Cheng stu on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct DragAndDropTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
